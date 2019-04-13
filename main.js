3fb8808f36addc0799f3e42d357d0b621e9e150c
onizu-db <39452934+onizu-db@users.noreply.github.com>
2019-04-14 03:23:15 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,80 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+let addWindow;
+
+// listen for the app to be ready
+app.on('ready', function(){
+  //create new window
+  mainWindow = new BrowserWindow({});
+  // load html file into window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Quit app when closed
+  mainWindow.on('closed', function() {
+    app.quit();
+  });
+
+  //Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert Menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle create add window
+function createAddWindow() {
+  //create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add list item'
+  });
+  // load html file into window
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  addWindow.on('close', function() {
+    addWindow = null;
+  });
+}
+
+// create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear Items'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+/*
+// If Mac, add empty object to Menu
+if(process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+*/
